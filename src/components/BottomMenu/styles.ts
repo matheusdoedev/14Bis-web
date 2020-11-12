8ed33c80ce30ee97c597198b1376754e21b797ff
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-11-12 13:50:16 -0300

    :lipstick: feat: Bottom Menu created

---
+++ src/components/BottomMenu/styles.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+// components
+import { Link } from "react-router-dom";
+
+export const BottomMenuContainer = styled.nav`
+  & {
+    background: var(--gradient);
+    border-radius: 35px 35px 0 0;
+    padding: var(--gap-md) 0;
+    width: 100vw;
+
+    position: absolute;
+    top: 99vh;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+`;
+
+export const BottomMenuItem = styled(Link)`
+  & {
+    padding: 0;
+
+    display: flex;
+    flex-direction: column;
+    place-content: center;
+
+    p {
+      color: white;
+      font-weight: 400;
+      margin-top: var(--gap);
+    }
+  }
+`;
