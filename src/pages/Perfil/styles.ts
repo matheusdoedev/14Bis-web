b0c19e548cc4483c01cb28b2a908351512d72835
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-11-12 17:00:06 -0300

    :lipstick: feat: Perfil page created

---
+++ src/pages/Perfil/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const PerfilContainer = styled.main`
+  & {
+  }
+`;
+
+export const PerfilHeader = styled.header`
+  & {
+    background: var(--gradient);
+    height: 107px;
+  }
+`;
