3bb1abb521327fda2e99aa57939adfb02e173d69
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-11-12 17:02:56 -0300

    :construction: chore: "Triagem" form made

--- src/pages/Triagem/index.tsx
+++ src/pages/Triagem/index.tsx
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { FormEvent } from "react";
+// hooks
+import { useHistory } from "react-router-dom";
 // styles
 import { TriagemContainer, TriagemForm } from "./styles";
 // components
 import { Container } from "../../styles/objects/container";
 import { Button } from "../../styles/objects/button";
+import Input from "../../components/Input";
+import CheckboxList from "../../components/Checkbox-list";
 
 const Triagem = () => {
+  const history = useHistory();
+
+  const gambiarraSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    history.push("/inicio");
+  };
+
   return (
     <TriagemContainer>
       <Container>
-        <TriagemForm>
-          <h1>Como podemos ajudar sua empresa?</h1>
+        <TriagemForm onSubmit={gambiarraSubmit}>
+          <h1>Qual é o seu perfil?</h1>
 
-          <Button to="/inicio">Continuar</Button>
+          <fieldset>
+            <h2>Sou um:</h2>
+            <CheckboxList
+              name="perfil"
+              required
+              options={[
+                { value: "empreendedor", label: "Empreendedor" },
+                { value: "mentor", label: "Mentor/Consultor" },
+                { value: "futuro-empreendedor", label: "Futuro empreendedor" },
+              ]}
+            />
+          </fieldset>
+
+          <fieldset>
+            <h2>Você já possui uma empresa/startup?</h2>
+            <CheckboxList
+              name="possui-empresa-startup"
+              required
+              options={[
+                { value: "sim", label: "Sim" },
+                { value: "não", label: "Não" },
+              ]}
+            />
+          </fieldset>
+          <Button as="button" type="submit">
+            Continuar
+          </Button>
         </TriagemForm>
       </Container>
     </TriagemContainer>
