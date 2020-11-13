94ba8820fc8d68666ea6c4e01c61c94fc8540d4e
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-11-13 12:43:25 -0300

    :construction: chore: some routes added
    Perfil, StartupEditarDados, MentorPerfil, Notificacoes, Notificacao, TermosDeUso and Estatistica

--- src/routes.tsx
+++ src/routes.tsx
@@ -12,16 +12,24 @@ import Investimento from "./pages/Investimento";
 import Forum from "./pages/Fórum";
 import NossosParceiros from "./pages/Nossos-parceiros";
 import Comunidades from "./pages/Comunidade";
-import Perfil from "./pages/Perfil";
+import Perfil from "./pages/Startup-perfil";
+import StartupEditarDados from "./pages/Startup-editar-dados";
+import MentorPerfil from "./pages/Mentor-perfil";
+import Notificacoes from "./pages/Notificacoes";
+import Notificacao from "./pages/Notificacao";
+import TermosDeUso from "./pages/Termos-de-uso";
+import Estatistica from "./pages/Estatistica";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
+        {/* cadastro e login */}
         <Route path="/" exact component={Home} />
         <Route path="/cadastro" component={Cadastro} />
         <Route path="/triagem" component={Triagem} />
         <Route path="/inicio" component={Inicio} />
+        {/* 14Bis */}
         <Route path="/quem-somos" component={QuemSomos} />
         <Route path="/consultorias" component={Consultorias} />
         <Route path="/mentorias" component={Mentorias} />
@@ -29,7 +37,19 @@ const Routes = () => {
         <Route path="/comunidade" component={Comunidades} />
         <Route path="/forum" component={Forum} />
         <Route path="/nossos-parceiros" component={NossosParceiros} />
-        <Route path="/perfil" component={Perfil} />
+        {/* startups routes */}
+        <Route path="/startups/termos-de-uso" component={TermosDeUso} />
+        <Route path="/startups/:id/perfil" component={Perfil} />
+        <Route
+          path="/startups/:id/editar-dados"
+          component={StartupEditarDados}
+        />
+        <Route path="/startups/:id/notificacoes/:id" component={Notificacao} />
+        <Route path="/startups/:id/notificacoes" component={Notificacoes} />
+        <Route path="/startups/:id/estatisticas" component={Estatistica} />
+        {/* mentores routes */}
+        <Route path="/mentores/:id/perfil" component={MentorPerfil} />
+        {/* <Route path="/mentores/:id/editar-dados" component={Perfil} /> */}
       </Switch>
     </BrowserRouter>
   );
